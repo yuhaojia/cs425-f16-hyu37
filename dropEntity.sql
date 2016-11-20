74f9649aebf0fd0b00ccb40c52bd524e71ca4880
Zheng You Thinkpad <zyou3@hawk.iit.edu>
2016-11-20 17:54:01 -0600

    drop

---
+++ dropEntity.sql
@@ -0,0 +1,10 @@
+drop table Address;
+drop table Staff;
+drop table Product;
+drop table Info;
+drop table CreditCard;
+drop table Customer;
+drop table ProOrder;
+drop table WareHouse;
+drop table Supplier;
+drop table SupplierProduct;

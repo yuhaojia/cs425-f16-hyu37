8a942e99dd3f8b0a62eb4c1bf439e1f1fdf39ed5
Zheng You Thinkpad <zyou3@hawk.iit.edu>
2016-11-20 16:57:13 -0600

    Tiny

--- hyu37.sql
+++ hyu37.sql
@@ -1,35 +1,29 @@
-
 --Entity
 --CreditCard Entity
-
 create table CreditCard (
 	cardNum numeric(16,0),
 	expDate date,
 	holder varchar(256),
 
-	primary key cardNum
+	primary key (cardNum)
 );
 
 --Customer Entity
-
 create table Customer (
 	customerID int,
 	balance float,
-	lastName char(256),
-	firstName char(256),
+	lastName varchar(256),
+	firstName varchar(256),
 
-	primary key customerID
+	primary key (customerID)
 );
 
 --Order Entity
-
 create table Order (
 	orderID int,
-	status varchar(256),
-	check(status in ('issued', 'send', 'recieved'))
-	
+	status varchar(256)	check(in ('issued', 'send', 'recieved')),	
 
-	primary key orderID
+	primary key (orderID)
 );
 
 --Relation

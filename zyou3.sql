5c20f05ec5f140096511a5895dae16ac66c4d422
Zheng You Thinkpad <zyou3@hawk.iit.edu>
2016-11-20 17:01:44 -0600

    Add not null

--- zyou3.sql
+++ zyou3.sql
@@ -1,21 +1,20 @@
 --entity Address
 create table Address(
 	addrID int,
-	streetNo int,
-	street varchar(256),
-	city varchar(256),
-	state varchar(256),
-	zipcode int,
-	addrType varchar(256),	--delivery, payment, product, staff, supplier
+	street varchar(256) not null,
+	city varchar(256) not null,
+	state varchar(256) not null,
+	zipcode int not null,
+	addrType varchar(256) not null,	--delivery, payment, product, staff, supplier
 	primary key(addrID)
 );
 
 --entity Staff
 create table Staff(
 	staffID int,
-	jobTitle varchar(256),
-	lastName varchar(256),
-	firstName varchar(256),
+	jobTitle varchar(256) not null,
+	lastName varchar(256) not null,
+	firstName varchar(256) not null,
 	primary key(staffID)
 );
 
@@ -24,6 +23,6 @@ create table Product(
 	proType varchar(256),
 	name varchar(256),
 	addInfo varchar(2048),
-	proSize float,
+	proSize float not null,
 	primary key(proType, name)
 );

b2038de85e5788994ce340b4805da68bc094a176
Zheng You Thinkpad <zyou3@hawk.iit.edu>
2016-11-20 18:35:11 -0600

    Add relationship

--- zyou3.sql
+++ zyou3.sql
@@ -1,3 +1,4 @@
+--entity
 --entity Address
 create table Address(
 	addrID int,
@@ -6,7 +7,9 @@ create table Address(
 	state varchar(256) not null,
 	zipcode int not null,
 	addrType varchar(256) not null,	--delivery, payment, product, staff, supplier
-	primary key(addrID)
+	staffID int,
+	primary key(addrID),
+	foreign key(staffID) references Staff(staffID)
 );
 
 --entity Staff
@@ -32,8 +35,49 @@ create table Product(
 create table Info(
 	infoType varchar(256),
 	info varchar(2048),
-	primary key(infoType, info)
+	proType varchar(256),
+	proName varchar(256),
+	primary key(infoType, info),
+	foreign key(proType, proName) references Product(proType, proName)
 );
+
+--relationship
+--relationship associate many-to-many
+create table associate(
+	customerID int,
+	addrID int,
+	primary key(customerID, addrID),
+	foreign key(customerID) references Customer(customerID),
+	foreign key(addrID) references Address(addrID)
+);
+
+--relationship liveIn one-to-many
+--add PK of Staff (one side) to Address (many side)
+
+--relationship pricePerState many-to-many
+create table pricePerState(
+	addrID int,
+	proType varchar(256),
+	proName varchar(256),
+	statePrice float,
+	primary key(addrID, proType, proName),
+	foreign key(addrID) references Address(addrID),
+	foreign key(proType, proName) references Product(proType, proName)
+);
+
+--relationship contain many-to-many
+create table contain(
+	orderID int,
+	proType varchar(256),
+	proName varchar(256),
+	quantity int,
+	primary key(orderID, proType, proName),
+	foreign key(orderID) references ProOrder(orderID),
+	foreign key(proType, proName) references Product(proType, proName)
+);
+
+--relationship has one-to-many
+--add PK of Product (one side) to Info (many side)
 
 drop table Address;
 drop table Staff;

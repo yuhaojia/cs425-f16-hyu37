2087631c77a5efece2e5ffb6a237f839d4701acf
Zheng You Thinkpad <zyou3@hawk.iit.edu>
2016-11-30 17:44:41 -0600

    Add more restriction

--- zyou3/src/service/UserService.java
+++ zyou3/src/service/UserService.java
@@ -13,39 +13,50 @@ public class UserService {
 	}
 	
 	public boolean validateCustomer(User u){
-		try{
-			pstmt = conn.prepareStatement("select * from Customer where customerID = ?");
-			pstmt.setString(1, String.valueOf(u.getID()));
-			ResultSet rs = pstmt.executeQuery();
-			if(rs.next()){
-				return true;
+		if(u.getType().equals("Customer") || u.getType().equals("customer")){
+			try{
+				pstmt = conn.prepareStatement("select * from Customer where customerID = ?");
+				pstmt.setString(1, String.valueOf(u.getID()));
+				ResultSet rs = pstmt.executeQuery();
+				if(rs.next()){
+					return true;
+				}
+				else{
+					return false;
+				}
 			}
-			else{
+			catch(SQLException e){
+				e.printStackTrace();
 				return false;
 			}
 		}
-		catch(SQLException e){
-			e.printStackTrace();
+		else{
 			return false;
-		}		
+		}
+
 	}
 	
 	public boolean validateStaff(User u){
-		try{
-			pstmt = conn.prepareStatement("select * from Staff where staffID = ?");
-			pstmt.setString(1, String.valueOf(u.getID()));
-			ResultSet rs = pstmt.executeQuery();
-			if(rs.next()){
-				return true;
+		if(u.getType().equals("Staff") || u.getType().equals("staff")){
+			try{
+				pstmt = conn.prepareStatement("select * from Staff where staffID = ?");
+				pstmt.setString(1, String.valueOf(u.getID()));
+				ResultSet rs = pstmt.executeQuery();
+				if(rs.next()){
+					return true;
+				}
+				else{
+					return false;
+				}
 			}
-			else{
+			catch(SQLException e){
+				e.printStackTrace();
 				return false;
 			}
 		}
-		catch(SQLException e){
-			e.printStackTrace();
+		else{
 			return false;
-		}		
+		}
 	}
 	
 }

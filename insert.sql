ff38aec30350f4c6b8b0aa60b7b70163be433c9f
Zheng You Thinkpad <zyou3@hawk.iit.edu>
2016-11-30 16:35:00 -0600

    half way insert

---
+++ insert.sql
@@ -0,0 +1,18 @@
+insert into address values(1, 'King Dr', 'Chicago', 'IL', 60616, 'delivery');
+insert into address values(2, 'China Pl', 'Chicago', 'IL', 60616, 'delivery');
+insert into address values(3, 'King Dr', 'Chicago', 'IL', 60616, 'payment');
+insert into address values(4, 'China Pl', 'Chicago', 'IL', 60616, 'payment');
+insert into address values(5, 'Federal Street', 'Chicago', 'IL', 60616, 'product');
+insert into address values(6, 'Michgan Street', 'Chicago', 'IL', 60616, 'product');
+insert into address values(7, 'King Dr', 'Chicago', 'IL', 60616, 'staff');
+insert into address values(8, 'China Pl', 'Chicago', 'IL', 60616, 'staff');
+insert into address values(9, 'Federal Street', 'Chicago', 'IL', 60616, 'supplier');
+insert into address values(10, 'Michgan Street', 'Chicago', 'IL', 60616, 'supplier');
+
+insert into Info values('Alcohol', '40%');
+insert into Info values('Vitamins', '10%');
+
+insert into Staff values(100, 'Manager', 'White', 'Jessy', 7);
+insert into Staff values(101, 'Assistant Manager', 'Smith', 'John', 8);
+
+insert into Project values('Food', 'Cookie', 0.5, 'Vitamins', );
